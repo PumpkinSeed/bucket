36fc264c844d70af19dccd149e3cce6e3dca2932
hajnalandor <hajnal.andor1993@gmail.com>
2019-09-23 15:58:11 +0200

    embedded structs WIP

--- operations.go
+++ operations.go
@@ -1,28 +1,46 @@
-package main
+package odatas
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
+	"github.com/rs/xid"
 	"reflect"
 )
 
-type order struct {
-	ordId      int
-	customerId int
-}
-
-func Write(s interface{}) {
-	if reflect.ValueOf(s).Kind() == reflect.Struct {
-		v := reflect.ValueOf(s)
-		for i:= 0; i < v.NumField(); i++ {
-			fmt.Println("field type",v.Type())
-			fmt.Println(v.Field(i))
+func Write(i interface{},typ string) error {
+	fields := make(map[string]interface{})
+	fields["ID"]= typ + "::" + xid.New().String()
+	if reflect.ValueOf(i).Kind() == reflect.Struct {
+		v := reflect.ValueOf(i)
+		for i := 0; i < v.NumField(); i++ {
+			if v.Field(i).Kind() == reflect.Struct {
+				//TODO
+				continue
+			}
+			k := v.Type().Field(i).Name
+			val := v.Field(i)
+			fields[k] = fmt.Sprintf("%v",val)
 		}
+
+		json, err := json.Marshal(fields)
+		if err != nil {
+			return err
+		}
+		fmt.Println(string(json))
+	} else {
+		return errors.New("not a struct")
 	}
+	fmt.Println("dass")
+	fmt.Println(fields["OrdId"])
+	fmt.Println(fields["CustomerId"])
+	fmt.Println(len(fields))
+
+	//handler.bucket.Insert(map["ID"],json)
+	return nil
 }
 
-func main() {
 
-}
 
 func Read() {
 
