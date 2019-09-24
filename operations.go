40005a17d3b412a9cc2924130a1df11af44cc7d2
hajnalandor <hajnal.andor1993@gmail.com>
2019-09-24 23:09:38 +0200

    Read VIP

--- operations.go
+++ operations.go
@@ -3,8 +3,10 @@ package odatas
 import (
 	"errors"
 	"fmt"
-	"github.com/couchbase/gocb"
 	"github.com/rs/xid"
+	"gopkg.in/couchbase/gocb.v1"
+	"strconv"
+
 	"reflect"
 )
 
@@ -79,6 +81,200 @@ func write(q interface{}, typ, id string) error {
 	return err
 }
 
-func Read(id, documentType string, result interface{}) {
+type a struct {
+	ID     int  `json:"id"`
+	UserID int  `json:"user_id"`
+	Name   Name `json:"name"`
+}
+type Name struct {
+	N string `json:"n"`
+}
 
+
+func readTest() error {
+	a := &a{
+		ID:     0,
+		UserID: 0,
+		Name: Name{
+			N: "123",
+		},
+	}
+	err := read("1","123",a)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func read(id,t string, ptr interface{}) error {
+	//documentID := t + "::" + id
+	var doc interface{}
+	// TODO
+	//_, err := placeholderBucket.Get(documentID, &doc)
+	//if err != nil {
+	//	return err
+	//}
+	type b struct {
+		ID string
+		UserID string
+		Name *Name
+		N string
+	}
+	doc = &b{
+		ID:     "12",
+		UserID: "23",
+		Name:   &Name{N: "Andor"},
+		N: "Andor",
+	}
+
+	typ := reflect.TypeOf(ptr).Elem()
+	val := reflect.ValueOf(ptr).Elem()
+
+	if typ.Kind() != reflect.Struct {
+		return errors.New("second argument must be a struct")
+	}
+
+	for i := 0; i < typ.NumField(); i++ {
+		typeField := typ.Field(i)
+		structField := val.Field(i)
+
+		if !structField.CanSet() {
+			fmt.Println("field ", i, "cannot be set")
+			continue
+		}
+
+		structFieldKind := structField.Kind()
+		//inputFieldName := typeField.Tag.Get("json")
+		inputFieldName := typeField.Name
+		if structFieldKind == reflect.Struct {
+			fmt.Println(structField.CanAddr())
+			err := read(id,inputFieldName, structField.Addr().Interface())//.Addr())//.Interface())
+			if err != nil {
+				return err
+			}
+			continue
+		}
+
+		if inputFieldName == "" {
+			inputFieldName = typeField.Name
+
+			if structFieldKind == reflect.Struct {
+				err := read( id,inputFieldName, structField.Addr().Interface())
+				if err != nil {
+					return err
+				}
+				continue
+			}
+		}
+		//ITT KOKANYOLTAM //TODO
+		// inputFieldName = "ID"
+		inputValue := fmt.Sprintf("%v",reflect.Indirect(reflect.ValueOf(doc)).FieldByName(inputFieldName).Interface())
+		fmt.Println(inputValue)
+		fmt.Printf("%T\n", inputValue)
+			if err := setWithProperType(typeField.Type.Kind(), inputValue, structField); err != nil {
+				return err
+			}
+
+	}
+	fmt.Println(ptr)
+	fmt.Println("fasz")
+	return nil
+}
+
+func setWithProperType(valueKind reflect.Kind, val string, structField reflect.Value) error {
+	switch valueKind {
+	case reflect.Int:
+		return setIntField(val, 0, structField)
+	case reflect.Int8:
+		return setIntField(val, 8, structField)
+	case reflect.Int16:
+		return setIntField(val, 16, structField)
+	case reflect.Int32:
+		return setIntField(val, 32, structField)
+	case reflect.Int64:
+		return setIntField(val, 64, structField)
+	case reflect.Uint:
+		return setUintField(val, 0, structField)
+	case reflect.Uint8:
+		return setUintField(val, 8, structField)
+	case reflect.Uint16:
+		return setUintField(val, 16, structField)
+	case reflect.Uint32:
+		return setUintField(val, 32, structField)
+	case reflect.Uint64:
+		return setUintField(val, 64, structField)
+	case reflect.Bool:
+		return setBoolField(val, structField)
+	case reflect.Float32:
+		return setFloatField(val, 32, structField)
+	case reflect.Float64:
+		return setFloatField(val, 64, structField)
+	case reflect.String:
+		structField.SetString(val)
+	case reflect.Ptr:
+		switch structField.Type().String() {
+		case "*int64":
+			return setPtrIntField(val, 64, structField)
+		case "*string":
+			structField.Set(reflect.ValueOf(&val))
+		default:
+			return errors.New("unknown type")
+		}
+
+	default:
+		return errors.New("unknown type")
+	}
+	return nil
+}
+
+func setPtrIntField(value string, bitSize int, field reflect.Value) error {
+	intVal, err := strconv.ParseInt(value, 10, bitSize)
+	if err == nil {
+		field.Set(reflect.ValueOf(&intVal))
+	}
+	return err
+}
+
+func setIntField(value string, bitSize int, field reflect.Value) error {
+	if value == "" {
+		value = "0"
+	}
+	intVal, err := strconv.ParseInt(value, 10, bitSize)
+	if err == nil {
+		field.SetInt(intVal)
+	}
+	return err
+}
+
+func setUintField(value string, bitSize int, field reflect.Value) error {
+	if value == "" {
+		value = "0"
+	}
+	uintVal, err := strconv.ParseUint(value, 10, bitSize)
+	if err == nil {
+		field.SetUint(uintVal)
+	}
+	return err
+}
+
+func setBoolField(value string, field reflect.Value) error {
+	if value == "" {
+		value = "false"
+	}
+	boolVal, err := strconv.ParseBool(value)
+	if err == nil {
+		field.SetBool(boolVal)
+	}
+	return err
+}
+
+func setFloatField(value string, bitSize int, field reflect.Value) error {
+	if value == "" {
+		value = "0.0"
+	}
+	floatVal, err := strconv.ParseFloat(value, bitSize)
+	if err == nil {
+		field.SetFloat(floatVal)
+	}
+	return err
 }
