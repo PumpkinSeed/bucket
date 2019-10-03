18f15f0258a1a9bf7f94f001defc6271234e7e40
hajnalandor <hajnal.andor1993@gmail.com>
2019-10-03 14:32:44 +0200

    add state tests

--- state_test.go
+++ state_test.go
@@ -1,6 +1,9 @@
 package bucket
 
-import "testing"
+import (
+	"github.com/rs/xid"
+	"testing"
+)
 
 func TestUpdateState(t *testing.T) {
 	s, err := newState(&Configuration{
@@ -35,5 +38,47 @@ func TestValidate(t *testing.T) {
 	_, err := th.ValidateState()
 	if err != nil {
 		t.Fatal(err)
+	}
+}
+
+func TestLoad(t *testing.T) {
+	if err := th.state.load(); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestInspect(t *testing.T) {
+	_ = th.state.setType("tsinspect", "tsinspect")
+	if b := th.state.inspect("tsinspect"); !b {
+		t.Error("type not found")
+	}
+	_ = th.state.deleteType("webshop")
+
+}
+
+func TestSetType(t *testing.T) {
+	if err := th.state.setType("webshop", "webshop"); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestDeleteType(t *testing.T) {
+	err := th.state.deleteType("webshop")
+	if err != nil {
+		t.Error(err)
+	}
+	_ = th.state.setType("webshop", "webshop")
+}
+
+func TestFetchDocIdentifierEmptyDocumentKey(t *testing.T) {
+	if s := th.state.fetchDocumentIdentifier(""); s != "" {
+		t.Errorf("error should be %s instead of %s", "", s)
+	}
+}
+
+func TestFetchDocIdentifier(t *testing.T) {
+	id := xid.New().String()
+	if s := th.state.fetchDocumentIdentifier("ws::" + id); s != id {
+		t.Errorf("error should be %s instead of %s", id, s)
 	}
 }
