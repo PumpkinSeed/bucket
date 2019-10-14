d990ce9618e30baf975ddf34462a699931fe57f4
Robert Boros <robbie13.boros@gmail.com>
2019-10-14 17:35:23 +0200

    adding multi node docker config, init.sh edited

--- Makefile
+++ Makefile
@@ -4,3 +4,7 @@ testing:
 dev:
 	docker-compose -f test/docker-compose.yml down
 	docker-compose -f test/docker-compose.yml up -d
+
+dev-multi:
+	docker-compose -f test/docker-compose-multi-node.yml down
+	docker-compose -f test/docker-compose-multi-node.yml up -d
--- test/docker/couchbase/init.sh
+++ test/docker/couchbase/init.sh
@@ -18,14 +18,27 @@ echo "launch couchbase"
 # wait for couchbase to be up - `couchbase-cli server-info` is broken / n/a
 wait_for_start wget -q localhost:8091
 
-# init the cluster - get_all_the_things
-couchbase-cli cluster-init -c 127.0.0.1:8091 -u Administrator -p password \
---cluster-username=Administrator --cluster-password=password \
---cluster-ramsize=600 --cluster-index-ramsize=512 \
---index-storage-setting=default --services=data,index,query,fts
+echo $CLUSTER_TYP + " setup"
 
-# create bucket
-couchbase-cli bucket-create -c 127.0.0.1:8091 -u Administrator -p password \
---bucket=company --bucket-type=couchbase --bucket-ramsize=600 --enable-flush=1 --wait
+if [ $CLUSTER_TYPE == "MASTER" ]
+  then
+    # init the cluster - get_all_the_things
+    couchbase-cli cluster-init -c 127.0.0.1:8091 -u Administrator -p password \
+    --cluster-username=Administrator --cluster-password=password \
+    --cluster-ramsize=600 --cluster-index-ramsize=512 \
+    --index-storage-setting=default --services=data,index,query,fts
+
+    # create bucket
+    couchbase-cli bucket-create -c 127.0.0.1:8091 -u Administrator -p password \
+    --bucket=company --bucket-type=couchbase --bucket-ramsize=600 --enable-flush=1 --wait
+elif [ $CLUSTER_TYPE == "REPLICA" ]; then
+    # add server to cluster
+    couchbase-cli server-add -c couchbase_master:8091 -u Administrator -p password \
+    --server-add="$(hostname -I | xargs):8091" --server-add-username=Administrator \
+    --server-add-password=password --services=data,index,query,fts
+
+    couchbase-cli rebalance -c couchbase_master:8091 -u Administrator -p password
+fi
+
 
 wait
